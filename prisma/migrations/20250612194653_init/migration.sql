c91b63bf578d3887a8740ee2152943599a6a3706
Tulio <tulioluv@gmail.com>
2025-06-12 17:47:04 -0300

    feat: imageUrl column added in database

---
+++ prisma/migrations/20250612194653_init/migration.sql
@@ -0,0 +1,37 @@
+/*
+  Warnings:
+
+  - Added the required column `imageUrl` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "imageUrl" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Cart" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CartItem" (
+    "id" SERIAL NOT NULL,
+    "cartId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "productName" TEXT NOT NULL,
+    "productPrice" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "CartItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "Cart"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -9,25 +9,29 @@ async function main() {
         name: "Wireless Mouse", 
         price: 29.99, 
         description: "Ergonomic wireless mouse with adjustable DPI and long battery life.", 
-        stock: 50 
+        stock: 50,
+        imageUrl: "https://m.media-amazon.com/images/I/61qpQ7ZsSmL.jpg"
       },
       { 
         name: "Mechanical Keyboard", 
         price: 89.99, 
         description: "RGB backlit mechanical keyboard with blue switches and customizable keys.", 
-        stock: 30 
+        stock: 30,
+        imageUrl: "https://www.bsavvi.co.uk/cdn/shop/files/k68-mechanical-gaming-keyboard-60percent-wireless-bluetooth-5-02-4ghz-bsavvi-1-34944275611941.jpg?v=1692923385"
       },
       { 
         name: "Noise-Cancelling Headphones", 
         price: 149.99, 
         description: "Over-ear noise-cancelling headphones with 20-hour battery life and Bluetooth 5.0.", 
-        stock: 20 
+        stock: 20,
+        imageUrl: "https://i5.walmartimages.com/seo/VILINICE-Noise-Cancelling-Headphones-Wireless-Bluetooth-Over-Ear-Headphones-with-Microphone-Black-Q8_b994b99c-835f-42fc-8094-9f6be0f9273b.be59955399cdbd1c25011d4a4251ba9b.jpeg"
       },
       { 
         name: "4K Monitor 27-inch", 
         price: 399.99, 
         description: "27-inch 4K UHD monitor with HDR support and ultra-thin bezels.", 
-        stock: 10 
+        stock: 10,
+        imageUrl: "https://www.hp.com/ca-en/shop/Html/Merch/Images/c08895796_1750x1285.jpg"
       }
     ]
   });
